6d4ad24820f8c6a4e51f02b1bc63d1440088e8da
everydayspring <lee.bom.952@gmail.com>
2024-10-14 11:24:32 +0900

    init

---
+++ src/main/java/com/sparta/springtrello/SpringTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springtrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrelloApplication.class, args);
+    }
+
+}
