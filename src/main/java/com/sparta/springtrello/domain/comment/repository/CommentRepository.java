b92b1f615010fa9f91652c57daa181cdf64581f7
pookyspooky <union0802@naver.com>
2024-10-15 04:04:47 +0900

    code format

--- src/main/java/com/sparta/springtrello/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/springtrello/domain/comment/repository/CommentRepository.java
@@ -1,11 +1,7 @@
 package com.sparta.springtrello.domain.comment.repository;
 
-import java.util.List;
-
+import com.sparta.springtrello.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sparta.springtrello.domain.comment.entity.Comment;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByCardId(Long cardId);
 }
